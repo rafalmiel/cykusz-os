fee5acc1fbb4dad8f5f729270beca9db938de43f
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2014-08-11 14:28:49 +0200

    kernel: Hello World kernel

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+nasm -felf ./arch/x86/boot.S -o boot.o
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+qemu-system-i386 -kernel myos.bin
+
