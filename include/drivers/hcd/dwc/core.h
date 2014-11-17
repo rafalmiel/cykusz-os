21f0bae34c771e4d8bfc09b64472be3eb864d26b
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2014-11-18 00:29:25 +0100

    dwc_otg: Add remaining Core registers mappings

--- include/drivers/hcd/dwc/core.h
+++ include/drivers/hcd/dwc/core.h
@@ -5,11 +5,133 @@
 
 #define DWC_OTG_CHANNEL_COUNT 16
 
+typedef union dwc_otg_core_gotgctl_reg {
+	volatile u32 raw;
+
+	volatile struct {
+		volatile const u32 sesreqscs : 1;
+		volatile u32 sesreq : 1;
+		volatile u32 vbvalidoven:1;
+		volatile u32 vbvalidovval:1;
+		volatile u32 avalidoven:1;
+		volatile u32 avalidovval:1;
+		volatile u32 bvalidoven:1;
+		volatile u32 bvalidovval:1;
+		volatile const u32 hstnegscs:1;
+		volatile u32 hnpreq:1;
+		volatile u32 host_set_hnp_enable : 1;
+		volatile u32 devhnpen:1;
+		volatile u32 __reserved12_15:4;
+		volatile const u32 conidsts:1;
+		volatile const u32 dbnctime:1;
+		volatile const u32 a_session_valid : 1;
+		volatile const u32 b_session_valid : 1;
+		volatile const u32 otg_version : 1;
+		volatile u32 __reserved21:1;
+		volatile u32 multvalidbc:5;
+		volatile u32 chirpen:1;
+		volatile u32 __reserved28_31:4;
+	} data;
+} dwc_otg_core_gotgctl_reg_t;
+
+typedef union dwc_otg_core_gotgint_reg {
+	volatile u32 raw;
+
+	volatile struct {
+		volatile u32 __reserved0_1 : 2;
+		volatile const u32 session_end_detected : 1;
+		volatile u32 __reserved3_7 : 5;
+		volatile const u32 session_request_success_status_change : 1;
+		volatile const u32 host_negotiation_success_status_change : 1;
+		volatile u32 __reserved10_16 : 7;
+		volatile const u32 host_negotiation_detected : 1;
+		volatile const u32 a_device_timeout_change : 1;
+		volatile const u32 debounce_done : 1;
+		volatile u32 __reserved20_31 : 12;
+	} data;
+} dwc_otg_core_gotgint_reg_t;
+
+typedef union dwc_otg_core_grstctl_reg {
+	volatile u32 raw;
+
+	volatile struct {
+		volatile u32 core_soft : 1;
+		volatile u32 hclk_soft : 1;
+		volatile u32 host_frame_counter : 1;
+		volatile u32 in_token_queue_flush : 1;
+		volatile u32 receive_fifo_flush : 1;
+		volatile u32 transmit_fifo_flush : 1;
+		volatile enum CoreFifoFlush {
+			transmit_fifo_flush_non_periodic = 0,
+			transmit_fifo_flush_periodic1 = 1,
+			transmic_fifo_flush_periodic2 = 2,
+			transmic_fifo_flush_periodic3 = 3,
+			transmic_fifo_flush_periodic4 = 4,
+			transmic_fifo_flush_periodic5 = 5,
+			transmic_fifo_flush_periodic6 = 6,
+			transmic_fifo_flush_periodic7 = 7,
+			transmic_fifo_flush_periodic8 = 8,
+			transmic_fifo_flush_periodic9 = 9,
+			transmic_fifo_flush_periodic10 = 10,
+			transmic_fifo_flush_periodic11 = 11,
+			transmic_fifo_flush_periodic12 = 12,
+			transmic_fifo_flush_periodic13 = 13,
+			transmic_fifo_flush_periodic14 = 14,
+			transmic_fifo_flush_periodic15 = 15,
+			transmic_fifo_flush_all = 16,
+		} transmit_fifo_flush_number : 5;
+		volatile u32 __reserved11_29 : 19;
+		volatile u32 dma_request_signal : 1;
+		volatile u32 abh_master_idle : 1;
+	} data;
+} dwc_otg_core_grstctl_reg_t;
+
+typedef union dwc_otg_core_ints_reg {
+
+	volatile u32 raw;
+
+	volatile struct {
+		volatile u32 current_mode : 1;
+		volatile u32 mode_mismatch : 1;
+		volatile u32 otg : 1;
+		volatile u32 dma_start_of_frame : 1;
+		volatile u32 receive_status_level : 1;
+		volatile u32 np_transmit_fifo_empty : 1;
+		volatile u32 ginnakeff:1;
+		volatile u32 goutnakeff:1;
+		volatile u32 ulpick:1;
+		volatile u32 i2c : 1;
+		volatile u32 early_suspend : 1;
+		volatile u32 usb_suspend : 1;
+		volatile u32 usb_reset : 1;
+		volatile u32 enumeration_done : 1;
+		volatile u32 isochronous_out_drop : 1;
+		volatile u32 eopframe:1;
+		volatile u32 restore_done : 1;
+		volatile u32 endpoint_mismatch : 1;
+		volatile u32 in_endpoint : 1;
+		volatile u32 out_endpoint : 1;
+		volatile u32 incomplete_isochronous_in : 1;
+		volatile u32 incomplete_isochronous_out : 1;
+		volatile u32 fetsetup:1;
+		volatile u32 reset_detected : 1;
+		volatile u32 port : 1;
+		volatile u32 host_channel : 1;
+		volatile u32 hp_transmit_fifo_empty : 1;
+		volatile u32 low_power_mode_transmit_received : 1;
+		volatile u32 connection_id_status_change : 1;
+		volatile u32 disconnect : 1;
+		volatile u32 session_request : 1;
+		volatile u32 wakeup : 1;
+	} data;
+
+} dwc_otg_core_ints_reg_t;
+
 typedef union dwc_otg_core_ghwcfg1_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile const enum endpoint_dir {
 			endpoint_dir_bidir = 0,
 			endpoint_dir_in = 1,
@@ -37,9 +159,9 @@ typedef union dwc_otg_core_ghwcfg1_reg {
 
 typedef union dwc_otg_core_ghwcfg2_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile const enum {
 			otgmode_hnp_srp_cap = 0,
 			otgmode_srp_cap = 1,
@@ -74,9 +196,9 @@ typedef union dwc_otg_core_ghwcfg2_reg {
 
 typedef union dwc_otg_core_ghwcfg3_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile const u32 transfer_size_width : 4;
 		volatile const u32 packet_size_width : 3;
 		volatile const u32 otgen : 1;
@@ -95,9 +217,9 @@ typedef union dwc_otg_core_ghwcfg3_reg {
 
 typedef union dwc_otg_core_ghwcfg4_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile const u32 periodic_in_endpoint_count : 4;
 		volatile const u32 partial_power_down : 1;
 		volatile const u32 min_abh_frequency : 1;
@@ -121,9 +243,9 @@ typedef union dwc_otg_core_ghwcfg4_reg {
 
 typedef union dwc_otg_core_gahbcfg_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile u32 glob_int_mask : 1;
 		volatile enum {
 			axi_burst_length_4 = 0,
@@ -151,9 +273,9 @@ typedef union dwc_otg_core_gahbcfg_reg {
 
 typedef union dwc_otg_core_gusbcfg_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile u32 toutcal : 3;
 		volatile const u32 phy_interface : 1;
 		volatile const enum {
@@ -188,11 +310,111 @@ typedef union dwc_otg_core_gusbcfg_reg {
 
 } dwc_otg_core_gusbcfg_reg_t;
 
+typedef union dwc_otg_core_grxsts_reg {
+
+	volatile u32 raw;
+
+	volatile struct {
+		volatile u32 channel_number : 4;
+		volatile u32 bcnt : 11;
+		volatile u32 dpid : 2;
+		volatile enum {
+			packet_status_inpacket = 2,
+			packet_status_in_transfer_complete = 3,
+			packet_status_data_toggle_error = 5,
+			packet_status_channel_halted = 7
+		} packet_status : 4;
+		volatile u32 __reserved21_31 : 11;
+	} data;
+
+} dwc_otg_core_grxsts_reg_t;
+
+typedef union dwc_otg_core_fifosize_reg {
+
+	volatile u32 raw;
+
+	volatile struct {
+		volatile u32 start_address : 16;
+		volatile u32 depth : 16;
+	} data;
+
+} dwc_otg_core_fifosize_reg_t;
+
+typedef union dwc_otg_core_gnptxsts_reg {
+
+	volatile u32 raw;
+
+	volatile struct {
+		volatile u32 space_available : 16;
+		volatile u32 queue_space_available : 8;
+		volatile u32 terminate : 1;
+		volatile enum {
+			token_type_in_out = 0,
+			token_type_zero_length_out = 1,
+			token_type_ping_complete_split = 2,
+			token_type_channel_halt = 3
+		} token_type : 2;
+		volatile u32 channel : 4;
+		volatile u32 odd : 1;
+	} data;
+
+} dwc_otg_core_gnptxsts_reg_t;
+
+typedef union dwc_otg_core_gusbmdiocntl_reg {
+
+	volatile u32 raw;
+
+	volatile struct {
+		volatile const u32 read : 16;
+		volatile u32 clock_ratio : 4;
+		volatile u32 free_run : 1;
+		volatile u32 bithash_enable : 1;
+		volatile u32 mdc_write : 1;
+		volatile u32 mdo_write : 1;
+		volatile u32 _reserved24_30 : 7;
+		volatile const u32 busy : 1;
+	} data;
+
+} dwc_otg_core_gusbmdiocntl_reg_t;
+
+typedef union dwc_otg_core_gusbmdiogen_reg {
+
+	volatile u32 raw;
+
+	volatile union {
+		volatile const u32 mdio_read;
+		volatile u32 mdio_write;
+	} data;
+
+} dwc_otg_core_gusbmdiogenl_reg_t;
+
+typedef union dwc_otg_core_gusbvbusdrv_reg {
+
+	volatile u32 raw;
+
+	volatile struct {
+		volatile u32 session_end : 1; // @0
+		volatile u32 vbus_valid : 1; // @1
+		volatile u32 bsession_valid : 1; // @2
+		volatile u32 asession_valid : 1; // @3
+		volatile const u32 discharge_vbus : 1; // Read Only @4
+		volatile const u32 charge_vbus : 1; // Read Only @5
+		volatile const u32 drive_vbus : 1; // Read Only @6
+		volatile u32 disable_driving : 1; // @7
+		volatile u32 vbus_irq_enabled : 1; // @8
+		volatile const u32 vbus_irq : 1; // Cleared on Read! @9
+		volatile u32 __reserved10_15 : 6; // @10
+		volatile u32 axi_priority_level : 4; // @16
+		volatile u32 __reserved20_31 : 12; // @20
+	} data;
+
+} dwc_otg_core_gusbvbusdrv_reg_t;
+
 typedef union dwc_otg_host_hcfg_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile enum {
 			clock_rate_30_60MHz = 0,
 			clock_rate_48MHz = 1,
@@ -215,9 +437,9 @@ typedef union dwc_otg_host_hcfg_reg {
 
 typedef union dwc_otg_host_hfir_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile u32 interval : 16;
 		volatile u32 dynamic_frame_reload : 1;
 		volatile u32 __reserved17_31 : 15;
@@ -227,9 +449,9 @@ typedef union dwc_otg_host_hfir_reg {
 
 typedef union dwc_otg_host_hfnum_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile const u32 frame_number : 16;
 		volatile const u32 frame_remaining : 16;
 	} data;
@@ -238,9 +460,9 @@ typedef union dwc_otg_host_hfnum_reg {
 
 typedef union dwc_otg_host_hptxsts_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile const u32 space_available : 16;
 		volatile const u32 queue_space_available : 8;
 		volatile const u32 terminate : 1;
@@ -277,9 +499,9 @@ typedef enum {
 
 typedef union dwc_otg_host_hprt_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile const u32 connect : 1;
 		volatile const u32 connect_detected : 1;
 		volatile const u32 enable : 1;
@@ -301,9 +523,9 @@ typedef union dwc_otg_host_hprt_reg {
 
 typedef union dwc_otg_host_hcchar_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile u32 maximum_packet_size : 11;
 		volatile u32 endpoint_number : 4;
 		volatile usb_direction_t endpoint_direction : 1;
@@ -321,9 +543,9 @@ typedef union dwc_otg_host_hcchar_reg {
 
 typedef union dwc_otg_host_hcsplt_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile u32 port_address : 7;
 		volatile u32 hub_address : 7;
 		volatile enum {
@@ -341,9 +563,9 @@ typedef union dwc_otg_host_hcsplt_reg {
 
 typedef union dwc_otg_host_hctsiz_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		volatile u32 transfer_size : 19;
 		volatile u32 packet_count : 10;
 		volatile enum packet_id {
@@ -360,9 +582,9 @@ typedef union dwc_otg_host_hctsiz_reg {
 
 typedef union dwc_otg_host_int_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
-	struct {
+	volatile struct {
 		u32 transfer_complete : 1;
 		u32 halt : 1;
 		u32 abh_error : 1;
@@ -384,10 +606,10 @@ typedef union dwc_otg_host_int_reg {
 
 typedef struct dwc_otg_core_regs {
 	/** OTG Control and Status Register		(Offset: 0x0) */
-	volatile u32 gotgctl;
+	volatile dwc_otg_core_gotgctl_reg_t gotgctl;
 
 	/** OTG Interrupt Register			(Offset 0x4) */
-	volatile u32 gotgint;
+	volatile dwc_otg_core_gotgint_reg_t gotgint;
 
 	/** AHB Configuration Register			(Offset 0x8) */
 	volatile dwc_otg_core_gahbcfg_reg_t gahbcfg;
@@ -396,28 +618,28 @@ typedef struct dwc_otg_core_regs {
 	volatile dwc_otg_core_gusbcfg_reg_t gusbcfg;
 
 	/** Reset Register				(Offset 0x10) */
-	volatile u32 grstctl;
+	volatile dwc_otg_core_grstctl_reg_t grstctl;
 
 	/** Interrupt Register				(Offset 0x14) */
-	volatile u32 gintsts;
+	volatile dwc_otg_core_ints_reg_t gintsts;
 
 	/** Interrupt Mask Register			(Offset 0x18) */
-	volatile u32 gintmsk;
+	volatile dwc_otg_core_ints_reg_t gintmsk;
 
 	/**  Receive Status Debug Read Register		(Offset 0x1C) */
-	volatile u32 grxstsr;
+	volatile dwc_otg_core_grxsts_reg_t grxstsr;
 
 	/** Receive Status Read Pop Register		(Offset 0x20) */
-	volatile u32 grxstsp;
+	volatile dwc_otg_core_grxsts_reg_t grxstsp;
 
 	/** Receive FIFO Size Register			(Offset 0x24) */
 	volatile u32 grxfsiz;
 
 	/** Non-periodic Transmit FIFO Size Register	(Offset 0x28) */
-	volatile u32 gnptxfsiz;
+	volatile dwc_otg_core_fifosize_reg_t gnptxfsiz;
 
 	/** Non-periodic Transmit FIFO Queue Status	(Offset 0x2C) */
-	volatile u32 gnptxsts;
+	volatile dwc_otg_core_gnptxsts_reg_t gnptxsts;
 
 	/** Reserved */
 	volatile u32 __reserved_0x30_0x34;
@@ -458,13 +680,13 @@ typedef struct dwc_otg_core_regs {
 	// BCM2835 specific registers
 
 	/** bcm2835 MDIO interface control		(Offset 0x80 */
-	volatile u32 gusbmdiocntl;
+	volatile dwc_otg_core_gusbmdiocntl_reg_t gusbmdiocntl;
 
 	/** bcm2835 Data for MDIO interface		(Offset 0x84 */
-	volatile u32 gusbmdiogen;
+	volatile dwc_otg_core_gusbmdiogenl_reg_t gusbmdiogen;
 
 	/** bcm2835 Vbus and other Miscellaneous ctrls	(Offset 0x88 */
-	volatile u32 gusbvbusdrv;
+	volatile dwc_otg_core_gusbvbusdrv_reg_t gusbvbusdrv;
 
 	// BCM2835 specific registers end
 
@@ -472,10 +694,12 @@ typedef struct dwc_otg_core_regs {
 	volatile u32 __reserved_0x8C_0xFC[29];
 
 	/** Host Periodic Transmit FIFO Size Register	(Offset 0x100) */
-	volatile u32 hptxfsiz;
+	volatile dwc_otg_core_fifosize_reg_t hptxfsiz;
 
 	/** Device IN Endpoint Transmit FIFO Size Regs	(Offset 0x104) */
-	volatile u32 dieptxf[15];
+	volatile dwc_otg_core_fifosize_reg_t dieptxf[15];
+
+	volatile u32 __reserved0x140_0x400[176];
 } dwc_otg_core_regs_t;
 
 typedef struct dwc_otg_host_regs {
@@ -496,16 +720,16 @@ typedef struct dwc_otg_host_regs {
 	volatile dwc_otg_host_hptxsts_reg_t hptxsts;
 
 	/** Interrupt					(Offset 0x414) */
-	volatile u32 haint;
+	volatile dwc_otg_core_ints_reg_t haint;
 
 	/** Interrupt mask				(Offset 0x418 */
-	volatile u32 haintmsk;
+	volatile dwc_otg_core_ints_reg_t haintmsk;
 
 	/** Frame list					(Offset 0x41C) */
 	volatile u32 hflbaddr;
 
 	/** Reserved */
-	volatile u32 __reserved0x420_0x43C[0x440 - 0x420];
+	volatile u8 __reserved0x420_0x43C[0x440 - 0x420];
 
 	/** Port					(Offset 0x440) */
 	volatile dwc_otg_host_hprt_reg_t hprt;
@@ -543,7 +767,7 @@ typedef struct dwc_otg_host_regs {
 
 typedef union dwc_otg_power_reg {
 
-	u32 raw;
+	volatile u32 raw;
 
 	struct {
 		volatile u32 stop_pclock : 1;
