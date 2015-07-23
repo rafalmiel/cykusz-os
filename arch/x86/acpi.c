3846c30a8e2c6a3ec4720f426cc87c3ee470bab7
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2015-07-23 23:41:19 +0100

    apic: Print apic acpi entries

--- arch/x86/acpi.c
+++ arch/x86/acpi.c
@@ -16,10 +16,25 @@ static u8 checksum(void *addr, u32 size)
 	return s;
 }
 
-static acpi_rsdp_descriptor_t *check_rsdp_ptr(void *addr)
+static s32 check_header(void *addr, char *sig, u32 len)
+{
+	u8 *p = addr;
+	u32 l = *(u32*)(p+4);
+
+
+	if (memcmp(sig, p, len) == 0) {
+		if (checksum(p, l) == 0) {
+			return 0;
+		}
+	}
+
+	return -1;
+}
+
+static acpi_rsdp_desc_t *check_rsdp_ptr(void *addr)
 {
 	if (memcmp(addr, "RSD PTR ", 8) == 0) {
-		if (checksum(addr, sizeof(acpi_rsdp_descriptor_t)) == 0) {
+		if (checksum(addr, sizeof(acpi_rsdp_desc_t)) == 0) {
 			return addr;
 		}
 	}
@@ -29,7 +44,7 @@ static acpi_rsdp_descriptor_t *check_rsdp_ptr(void *addr)
 
 static void *find_rsdt_address(void)
 {
-	acpi_rsdp_descriptor_t *rsdp = 0;
+	acpi_rsdp_desc_t *rsdp = 0;
 	u32 rsdt_addr = 0;
 	u32 *s = (u32*)0xC00E0000;
 
@@ -61,6 +76,82 @@ static void *find_rsdt_address(void)
 	return (void *) (0);
 }
 
+static inline void log(const char *txt, u32 value)
+{
+	kprint(txt);
+	kprint(": ");
+	kprint_hexnl(value);
+}
+
+static void parse_matd(void *addr)
+{
+	u8 *a = addr;
+	u8 *limit = a;
+	acpi_matd_t *matd = (acpi_matd_t*)a;
+
+	kprint("----------\n");
+	kprint("APIC: Local controller address: ");
+	kprint_hexnl(matd->localcontrolleraddress);
+	kprint("APIC: Flags: ");
+	kprint_hexnl(matd->flags);
+
+	a += sizeof *matd;
+	acpi_matd_entry_t *e = (acpi_matd_entry_t *)a;
+
+	kprint_hexnl(e->lenght);
+	kprint_hexnl(e->type);
+
+	limit += matd->lenght;
+
+
+	while (a < limit) {
+		acpi_matd_entry_t *e = (acpi_matd_entry_t *)a;
+
+		kprint("APIC ENTRY TYPE: ");
+		kprint_hex(e->type);
+		kprint(" ");
+		kprint_hexnl(e->lenght);
+
+		a += 2;
+
+		switch (e->type) {
+		case 0:
+		{
+			acpi_matd_entry_local_apic_t *i = a;
+			log("        apic processor id", i->procid);
+			log("        apic id", i->apicid);
+			log("        flags", i->flags);
+
+			break;
+		}
+		case 1:
+		{
+			acpi_matd_entry_io_apic_t *i = a;
+			log("        io apic id", i->ioapicid);
+			log("        io apic address", i->ioapicaddress);
+			log("        global sys int base", i->globalintbase);
+			break;
+		}
+		case 2:
+		{
+			acpi_matd_entry_intsrc_t *i = a;
+			log("        bus source", i->bussrc);
+			log("        irq source", i->irqsrc);
+			log("        global sys int", i->globalsysint);
+			log("        flags", i->flags);
+			break;
+		}
+		default:
+			break;
+		}
+
+		if (e->lenght == 0)
+			return;
+
+		a += (e->lenght - 2);
+	}
+}
+
 void init_acpi(void)
 {
 	u32* rsdt_addr = find_rsdt_address();
@@ -77,6 +168,10 @@ void init_acpi(void)
 			paging_identity_map(*rsdt_addr);
 
 			u8 *p = (u8*)(*rsdt_addr);
+
+			if (check_header(p, "APIC", 4) == 0) {
+				parse_matd(p);
+			}
 
 			for (u32 i = 0; i < 4; ++i) {
 				kprint_char(*p++);
--- arch/x86/acpi.h
+++ arch/x86/acpi.h
@@ -5,13 +5,52 @@
 
 void init_acpi(void);
 
-typedef struct acpi_rsdp_descriptor {
+typedef struct acpi_rsdp_desc {
 	char signature[8];
 	u8 checksum;
 	char oemid[6];
 	u8 revision;
 	u32 rsdt_address;
-} __attribute__ ((packed)) acpi_rsdp_descriptor_t;
+} __attribute__ ((packed)) acpi_rsdp_desc_t;
+
+typedef struct acpi_matd {
+	char signature[4];
+	u32 lenght;
+	u8 revision;
+	u8 checksum;
+	char oemid[6];
+	char oemtableid[8];
+	u32 oemrevision;
+	u32 creatorid;
+	u32 creatorrevision;
+	u32 localcontrolleraddress;
+	u32 flags;
+} __attribute__((packed)) acpi_matd_t;
+
+typedef struct acpi_matd_entry {
+	u8 type;
+	u8 lenght;
+} __attribute__((packed)) acpi_matd_entry_t;
+
+typedef struct acpi_matd_entry_local_apic {
+	u8 procid;
+	u8 apicid;
+	u32 flags;
+} __attribute__((packed)) acpi_matd_entry_local_apic_t;
+
+typedef struct acpi_matd_entry_io_apic {
+	u8 ioapicid;
+	u8 reserved;
+	u32 ioapicaddress;
+	u32 globalintbase;
+} __attribute__((packed)) acpi_matd_entry_io_apic_t;
+
+typedef struct acpi_matd_entry_intsrc {
+	u8 bussrc;
+	u8 irqsrc;
+	u32 globalsysint;
+	u16 flags;
+} __attribute__((packed)) acpi_matd_entry_intsrc_t;
 
 #endif // ACPI_H
 
